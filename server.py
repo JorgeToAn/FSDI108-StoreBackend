67c188d68bde699c5b1207e0c994bac2d0140106
JorgeToAn <jorgetosta.f@gmail.com>
2022-08-15 20:00:23 -0700

    Class 3

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import Flask
 import json
 from data import me
+from data import catalog
 
 app = Flask(__name__)
 
@@ -29,6 +30,52 @@ def test_api():
 def about_api():
     return json.dumps(me)
 
+@app.get("/api/catalog")
+def get_catalog():
+    # return list of products
+    return json.dumps(catalog)
+
+@app.get("/api/product/<id>")
+def get_product_by_id(id):
+    product = "Product not found"
+    for item in catalog:
+        if(id == item["_id"]):
+            product = item
+            break
+    
+    return json.dumps(product)
+
+@app.get("/api/products/<category>")
+def get_products_by_category(category):
+    products = []
+    for item in catalog:
+        if(category.lower() == item["category"].lower()):
+            products.append(item)
+
+    return json.dumps(products)
+
+@app.get("/api/count")
+def get_catalog_count():
+    # returns the number of items in the catalog
+    count = len(catalog)
+    return json.dumps(count)
+
+@app.get("/api/catalog/total")
+def get_catalog_total():
+    total = 0
+    for item in catalog:
+        total += item["price"]
+    
+    return json.dumps(total)
+
+@app.get("/api/catalog/cheapest")
+def get_cheapest():
+    cheapest = catalog[0]
+    for item in catalog:
+        if(item["price"] < cheapest["price"]):
+            cheapest = item
+
+    return json.dumps(cheapest)
 
 
 
@@ -36,4 +83,4 @@ def about_api():
 
 
 # debug=True allows code to be loaded without needing to kill and restart the server
-app.run(debug=True)
+# app.run(debug=True)
